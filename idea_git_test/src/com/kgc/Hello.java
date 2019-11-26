2e44a2a209e1c5a1e09d8a8bf1382a4d661395c2
hjh <635892421@qq.com>
2019-11-26 15:39:22 +0800

    添加到本地仓库2

--- idea_git_test/src/com/kgc/Hello.java
+++ idea_git_test/src/com/kgc/Hello.java
@@ -3,5 +3,7 @@ package com.kgc;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("hhaha");
+        System.out.println("hhaha");
+        System.out.println("hhaha");
     }
 }
